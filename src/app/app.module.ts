e5d6de2b4160bbe5971ba48ea8fa780ca2d560fb
Kiie911 <62591869+Kiie911@users.noreply.github.com>
2020-10-08 09:55:39 +0700

    merge new code dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FriendlistComponent } from './friendlist/friendlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FriendlistComponent
   ],
   imports: [
     BrowserModule,
