c2dc50560975bd00e40d4828fbb4c8e6957b2b54
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-10-13 17:04:59 +0700

    merge dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,15 +18,15 @@ import {environment} from '../environments/environment';
 import {AngularFirestoreModule} from '@angular/fire/firestore';
 import {AngularFireStorage} from '@angular/fire/storage';
 import {authInterceptorProviders} from './auth.interceptor';
-import { StatusComponent } from './status/status.component';
-import { CommentComponent } from './comment/comment.component';
-import { CommentListComponent } from './comment-list/comment-list.component';
-import { UserInfoComponent } from './user-info/user-info.component';
-import { EditUserInfoComponent } from './edit-user-info/edit-user-info.component';
-import { EditComponent } from './edit/edit.component';
-import { EditPasswordComponent } from './edit-password/edit-password.component';
-import { CommentEditComponent } from './comment-edit/comment-edit.component';
-import { StatusEditComponent } from './status-edit/status-edit.component';
+import {StatusComponent} from './status/status.component';
+import {CommentComponent} from './comment/comment.component';
+import {CommentListComponent} from './comment-list/comment-list.component';
+import {UserInfoComponent} from './user-info/user-info.component';
+import {EditUserInfoComponent} from './edit-user-info/edit-user-info.component';
+import {EditComponent} from './edit/edit.component';
+import {EditPasswordComponent} from './edit-password/edit-password.component';
+import {CommentEditComponent} from './comment-edit/comment-edit.component';
+import {StatusEditComponent} from './status-edit/status-edit.component';
 
 
 @NgModule({
@@ -47,8 +47,7 @@ import { StatusEditComponent } from './status-edit/status-edit.component';
     EditUserInfoComponent,
     EditComponent,
     EditPasswordComponent,
-    CommentEditComponent
-    EditPasswordComponent,
+    CommentEditComponent,
     StatusEditComponent
   ],
   imports: [
