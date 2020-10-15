9fe3efd158a46535ef2a1fa7c4a97a094b2806d0
Kiie911 <62591869+Kiie911@users.noreply.github.com>
2020-10-15 14:12:31 +0700

    fix config firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,24 +6,14 @@ export const environment = {
   production: false,
   URL: 'http://localhost:8080/',
   firebase: {
-    // apiKey: "AIzaSyCyYMd9-zh3RgYfzrcCV4nEPLlAQJrYxXM",
-    // authDomain: "gzbook-4544e.firebaseapp.com",
-    // databaseURL: "https://gzbook-4544e.firebaseio.com",
-    // projectId: "gzbook-4544e",
-    // storageBucket: "gzbook-4544e.appspot.com",
-    // messagingSenderId: "148893928365",
-    // appId: "1:148893928365:web:3456cb6de9ee1d11b56f24",
-    // measurementId: "G-M61T5Y66HN"
-
-    apiKey: "AIzaSyBe3LorG-dDVWU85LnYfO4tzbQArpHR-8I",
-    authDomain: "porject-module6-vh2.firebaseapp.com",
-    databaseURL: "https://porject-module6-vh2.firebaseio.com",
-    projectId: "porject-module6-vh2",
-    storageBucket: "porject-module6-vh2.appspot.com",
-    messagingSenderId: "178858428950",
-    appId: "1:178858428950:web:910b29fa0622faa409c337",
-    measurementId: "G-7FV4FJY6NS"
-
+    apiKey: "AIzaSyAVADDtuS6L7ipGSG-MUaODQkURxTuPlc4",
+    authDomain: "gzbook-5fd15.firebaseapp.com",
+    databaseURL: "https://gzbook-5fd15.firebaseio.com",
+    projectId: "gzbook-5fd15",
+    storageBucket: "gzbook-5fd15.appspot.com",
+    messagingSenderId: "387249051927",
+    appId: "1:387249051927:web:619d1a594d56d732027f52",
+    measurementId: "G-NC44R8HE7E"
   }
 
 
